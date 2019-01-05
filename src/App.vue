54e90777859d08d6867aeba6bae9c724866b3922
syahrulutomo <irullvandalis@gmail.com>
2019-01-05 22:05:16 +0700

    navigation bar ready

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,186 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <!-- Menu collapse -->
+    <div v-bind:class="{ open: isOpen }" class="navbar-expanded" v-if="$mq === 'mobile'">
+			<ul class="menu-expanded">
+					<li class="menu-list-expanded"><a class="menu-item-expanded" href="#">Discover</a></li>
+					<li class="menu-list-expanded"><a class="menu-item-expanded" href="#">Genres</a></li>
+					<li class="menu-list-expanded"><a class="menu-item-expanded" href="#">Companies</a></li>
+			</ul>			
+		</div>
+
+    <!-- Navbar -->
+    <div class="header">
+			<a class="logo" href="#" v-if="$mq === 'mobile'">Tokoflix</a>
+      <a class="logoBig" href="#" v-if="$mq === 'desktop'">Tokoflix</a>
+			<nav class="navbar">
+				<ul class="menu" v-if="$mq === 'desktop'">
+					<li class="menu-list"><a class="menu-item" href="#">Discover</a></li>
+					<li class="menu-list"><a class="menu-item" href="#">Genres</a></li>
+					<li class="menu-list"><a class="menu-item" href="#">Companies</a></li>
+				</ul>			
+			</nav>
+				<a id="drawer" href="#"><img :src="images.drawerImg" v-on:click="showNavbarExpanded" 
+        v-if="$mq === 'mobile'"></a>
+    </div>
+
+    <Home/>
+ 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
+import Vue from 'vue'
+import VueMq from 'vue-mq'
+
+Vue.use(VueMq, {
+  breakpoints: {
+    mobile: 800,
+    desktop: Infinity,
+  }
+})
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Home
+  },
+  data(){
+    return{
+      images:{
+        drawerImg: require('./assets/menu-burger.png')
+      },
+      isOpen: false
+    }
+  },
+  methods:{
+    showNavbarExpanded: function(){
+      if(this.isOpen === false){
+          this.isOpen = true;
+      }else{
+        this.isOpen = false;
+      }
+    }
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css?family=Nunito:700');
+@import url('https://fonts.googleapis.com/css?family=Open+Sans');
+
+html, body{
+  margin: 0;
 }
+
+#app{
+  margin-top: 80px; 
+}
+
+div, footer, nav{
+	box-sizing: border-box;
+  margin: 0;
+}
+
+.header{
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding: .5rem 1rem;
+  background-color: #f4f4f4;
+  border-bottom: 1px solid #d9d9d9;
+  display: flex;
+  flex-wrap: wrap;    
+}
+
+.logo{
+  font-size: 2rem;
+  color: #42b549;
+  font-family: 'Nunito', sans-serif;
+  text-decoration: none;
+  text-shadow: 2px 3px 4px #d9d9d9;
+}
+
+.logoBig{
+  font-size: 2.2rem;
+  color: #42b549;
+  font-family: 'Nunito', sans-serif;
+  text-decoration: none;
+  text-shadow: 2px 3px 4px #d9d9d9;
+}
+
+.navbar{
+ margin-left: 16%;
+    padding: 0 1em;
+    vertical-align: baseline;
+}
+
+.menu{
+}
+
+.menu-list {
+  list-style: none;
+  display: inline;
+}
+
+.menu-item{
+  color: #606060;
+  padding: 1em;
+  font-size: 1rem;
+  font-family: 'Open Sans', sans-serif;
+  text-decoration: none;
+}
+
+.navbar-expanded {
+	width: 100%;
+	padding: .8rem 1rem;
+  margin-top: -1.25em;
+  background-color: #f4f4f4;
+  position: fixed;
+  z-index: 3;
+  /* This trasform moves the drawer off canvas. */
+  -webkit-transform: translate(0px, -200px);
+  transform: translate(0px, -200px);
+  /* Optionally, we animate the drawer. */
+}
+
+.menu-expanded{
+  padding: 0;
+  margin: 0;
+}
+
+.menu-list-expanded{
+	list-style: none;
+	margin-bottom: 1rem;
+}
+
+.menu-item-expanded{
+  font-family: 'Open Sans', sans-serif;
+	font-size: 1rem;
+	text-decoration: none;
+	color: #606060;
+  margin-left: .5rem;
+}
+
+.open {
+  -webkit-transform: translate(0, 0px);
+  transform: translate(0, 0px);
+}
+
+.drawer{
+	width: 50px;
+}
+
+#drawer img{
+	width: 30px;
+  position: absolute;
+  top: 10px;
+	right: 10px;	
+}
+
+
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home-container">
+    <>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data: function(){
+    return{
+      apiKey: c6d705fa0fcbcfbebd83c899a86bbcfb,
+      listHome: []
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
